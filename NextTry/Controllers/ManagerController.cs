02a5f2a59b2a1fb6718a47d1531cb652a8d73b1d
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2021-08-14 23:11:05 +0300

    Добавьте файлы проекта.

---
+++ NextTry/Controllers/ManagerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextTry.Controllers
+{
+    [Route("api/manager")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+    }
+}
